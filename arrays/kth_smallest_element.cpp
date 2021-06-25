3e2ee00812b32aad3da0512b81656c905873fa8a
Harsh <harsh.here17@gmail.com>
2021-06-25 12:58:25 +0530

    kth smallest element in array

---
+++ arrays/kth_smallest_element.cpp
@@ -0,0 +1,24 @@
+/**
+ * Time   - O(nLog(n))
+ * Space  - O(1)
+ * Author - 24Cipher
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution{
+    public:
+    // arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
+    int kthSmallest(int arr[], int l, int r, int k) {
+        //code here
+        sort(arr, arr+(r+1));
+        return arr[k-1];
+        // cout<<l<<" "<<r<<" "<<k;
+        
+    }
+};
